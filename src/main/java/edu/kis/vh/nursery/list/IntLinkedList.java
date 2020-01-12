defc75b1489140110406cfe7d0ef3798b38d59ec
Thomson19 <tomek.siekacz97@gmail.com>
2020-01-12 21:07:13 +0100

    Dodanie stalych, adnotacje @Override

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,6 +2,7 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
+    public static final int LIST_IS_EMPTY = -1;
     private Node last;
 
     public void push(int i) {
@@ -24,13 +25,13 @@ public class IntLinkedList {
 
     public int top() {
         if (isEmpty())
-            return -1;
+            return LIST_IS_EMPTY;
         return getLast().getValue();
     }
 
     public int pop() {
         if (isEmpty())
-            return -1;
+            return LIST_IS_EMPTY;
         int ret = getLast().getValue();
         setLast(getLast().getPrev());
         return ret;
