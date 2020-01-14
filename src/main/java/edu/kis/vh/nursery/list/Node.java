10720c8b1651e9946c8930685a9f42d1161af55c
smirnoffq <bartlomiej.gorkiewicz@gmail.com>
2020-01-14 09:39:22 +0100

    Adding documentation comments

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -6,30 +6,58 @@ public class Node {
     private Node prev;
     private Node next;
 
+    /**
+     * Contructor for node with value as i
+     * @param i - new node value
+     */
     public Node(int i) {
         setValue(i);
     }
 
+    /**
+     * Gets node value
+     * @return Node value
+     */
     public int getValue() {
         return value;
     }
 
+    /**
+     * Sets node value
+     * @param value
+     */
     public void setValue(int value) {
         this.value = value;
     }
 
+    /**
+     * Returns previous Node
+     * @return previous Node
+     */
     public Node getPrev() {
         return prev;
     }
 
+    /**
+     * Sets previous node
+     * @param prev previous node
+     */
     public void setPrev(Node prev) {
         this.prev = prev;
     }
 
+    /**
+     * Returns next Node
+     * @return next Node
+     */
     public Node getNext() {
         return next;
     }
 
+    /**
+     * Sets next Node
+     * @param next next node
+     */
     public void setNext(Node next) {
         this.next = next;
     }
